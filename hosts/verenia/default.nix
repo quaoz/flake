ced7aabfc56869e12246c7d05d94f1af38273276
quaoz <74419801+quaoz@users.noreply.github.com>
2025-09-15 03:19:40 +0100

    fix: define verenias tailnet addresses

--- hosts/verenia/default.nix
+++ hosts/verenia/default.nix
@@ -1,19 +1,4 @@
 {
-  garden = {
-    pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMOxWr7LsWITmXfclK0QVvYboKsZFYHKoFmvRHVtZWg";
-
-    profiles.server.enable = true;
-
-    system.boot.loader = "systemd-boot";
-
-    services = {
-    };
-
-    hardware = {
-      cpu = "intel";
-    };
-  };
-
   fileSystems = {
     "/" = {
       device = "/dev/disk/by-label/nixos";
@@ -30,4 +15,35 @@
   swapDevices = [
     {device = "/dev/disk/by-label/swap";}
   ];
+
+  garden = {
+    pubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICMOxWr7LsWITmXfclK0QVvYboKsZFYHKoFmvRHVtZWg";
+
+    profiles.server.enable = true;
+    services = {};
+
+    system.boot.loader = "systemd-boot";
+
+    hardware = {
+      cpu = "intel";
+    };
+
+    networking = {
+      wireless.enable = false;
+
+      addresses = {
+        internal = {
+          ipv4 = {
+            enable = true;
+            address = "100.64.0.3";
+          };
+
+          ipv6 = {
+            enable = true;
+            address = "fd7a:115c:a1e0::3";
+          };
+        };
+      };
+    };
+  };
 }
