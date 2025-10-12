bc291e4e08e43845a13a06e082d2920485bc1030
quaoz <74419801+quaoz@users.noreply.github.com>
2025-10-12 22:14:05 +0100

    feat: mailserver automx

---
+++ modules/nixos/services/mailserver/automx.nix
@@ -0,0 +1,53 @@
+{
+  self,
+  lib,
+  config,
+  ...
+}: let
+  baseDomain = config.garden.domain;
+  mailDomain = config.garden.services.mailserver.domain;
+  cfg = config.garden.services.automx;
+in {
+  options.garden.services.automx = self.lib.mkServiceOpt "automx" {
+    visibility = "public";
+    dependsLocal = ["nginx" "mailserver"];
+    proxy = false;
+    domain = "autoconfig.${baseDomain}";
+  };
+
+  config = lib.mkIf cfg.enable {
+    garden.magic.public.domains."${baseDomain}".extraRecords = [
+      {
+        type = "cname";
+        label = "autodiscover";
+        target = "${cfg.domain}.";
+      }
+    ];
+
+    security.acme.certs.${baseDomain}.extraDomainNames = ["autodiscover.${baseDomain}"];
+
+    services = {
+      nginx.virtualHosts."${cfg.domain}".enableACME = lib.mkForce false;
+
+      automx2 = {
+        enable = true;
+        domain = baseDomain;
+
+        settings = {
+          provider = "${baseDomain}";
+          domains = ["${baseDomain}"];
+          servers = [
+            {
+              type = "imap";
+              name = "${mailDomain}";
+            }
+            {
+              type = "smtp";
+              name = "${mailDomain}";
+            }
+          ];
+        };
+      };
+    };
+  };
+}
