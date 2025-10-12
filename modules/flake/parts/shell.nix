d9b8ec6c4983e4254aa4cb18d472ed2c6958b4bd
quaoz <74419801+quaoz@users.noreply.github.com>
2025-10-12 22:05:13 +0100

    chore: remove secret management commands

--- modules/flake/parts/shell.nix
+++ modules/flake/parts/shell.nix
@@ -26,34 +26,6 @@
 
       commands = [
         {
-          name = "rekey";
-          category = "secrets";
-          help = "re-encrypts secrets for hosts that require them";
-
-          command = "${lib.getExe config.agenix-rekey.package} rekey --add-to-git";
-        }
-        {
-          name = "decrypt";
-          category = "secrets";
-          help = "attempts to decrypt the specified secret";
-
-          command = ''
-            if [[ -f "''${1:-}" ]]; then
-                file=$1
-            else
-                if [[ -n "$FLAKE" && -d "$FLAKE/secrets" ]]; then
-                    secrets="$FLAKE/secrets"
-                else
-                    secrets="$(git rev-parse --show-toplevel)/secrets"
-                fi
-
-                file=$(${lib.getExe pkgs.gum} file "$secrets")
-            fi
-
-            ${lib.getExe pkgs.rage} -d -i ~/.ssh/id_ed25519 "$file"
-          '';
-        }
-        {
           name = "switch";
           category = "deploy";
           help = "builds and activates the system configuration";
