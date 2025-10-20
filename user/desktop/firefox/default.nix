805b67f53a5ef3baf054e4e0d0c6ddae073a9a58
quaoz <74419801+quaoz@users.noreply.github.com>
2025-10-21 00:58:12 +0100

    fix: don't break when buildbot tries to evaluate darwin systems

--- user/desktop/firefox/default.nix
+++ user/desktop/firefox/default.nix
@@ -4,9 +4,17 @@
   self,
   inputs,
   inputs',
+  config,
   osConfig,
   ...
 }: let
+  betterfox = pkgs.fetchFromGitHub {
+    owner = "yokoffing";
+    repo = "Betterfox";
+    rev = "140.0";
+    hash = "sha256-gHFA/1PeQ0iNAcjATGwgJOqRlR9YmxD/RJKkYN36QYA=";
+  };
+
   package =
     self.lib.ldTernary pkgs
     inputs'.firefox-nightly.packages.firefox-nightly-bin
@@ -22,191 +30,196 @@
       }) {}
     );
 in {
-  programs.firefox = lib.mkIf osConfig.garden.profiles.desktop.enable {
-    enable = true;
-    inherit package;
+  config = lib.mkIf osConfig.garden.profiles.desktop.enable {
+    home.file."${config.programs.firefox.profilesPath}/default/user.js".source = pkgs.concatText "user.js" [
+      (builtins.toFile "hm-user.js" config.home.file."${config.programs.firefox.profilesPath}/default/user.js".text)
+      "${betterfox}/user.js"
+      ./overrides.js
+    ];
 
-    profiles."default" = {
-      id = 0;
-      isDefault = true;
+    programs.firefox = {
+      enable = true;
+      inherit package;
 
-      settings = {
-        "extensions.autoDisableScopes" = 0;
-      };
+      profiles."default" = {
+        id = 0;
+        isDefault = true;
 
-      # use betterfox user.js
-      extraConfig = builtins.readFile "${pkgs.betterfox}/user.js" + builtins.readFile ./overrides.js;
-
-      # TODO: i don't actually want a lot of these
-      extensions.packages = with inputs'.firefox-addons.packages; [
-        # keep-sorted start
-        auto-tab-discard
-        bitwarden
-        blocktube
-        buster-captcha-solver
-        control-panel-for-twitter
-        cookies-txt
-        darkreader
-        dearrow
-        don-t-fuck-with-paste
-        header-editor
-        indie-wiki-buddy
-        justdeleteme
-        lovely-forks
-        search-by-image
-        sponsorblock
-        stylus
-        terms-of-service-didnt-read
-        translate-web-pages
-        ublacklist
-        ublock-origin
-        unpaywall
-        user-agent-string-switcher
-        userchrome-toggle-extended
-        violentmonkey
-        wayback-machine
-        web-scrobbler
-        zotero-connector
-        # keep-sorted end
-      ];
-
-      search = let
-        mkSearchFull = {
-          alias,
-          domain,
-          searchPath ? "/search",
-          searchParam ? "q",
-          extraParams ? [],
-        }: {
-          icon = "https://" + domain + "/favicon.ico";
-          updateInterval = 24 * 60 * 60 * 1000;
-
-          definedAliases = ["@${alias}"];
-          urls = [
-            {
-              template = "https://" + domain + searchPath;
-              params =
-                [
-                  {
-                    name = searchParam;
-                    value = "{searchTerms}";
-                  }
-                ]
-                ++ extraParams;
-            }
-          ];
+        settings = {
+          "extensions.autoDisableScopes" = 0;
         };
 
-        mkSearch' = alias: domain: extra:
-          mkSearchFull ({
-              inherit alias domain;
-            }
-            // extra);
+        # TODO: i don't actually want a lot of these
+        extensions.packages = with inputs'.firefox-addons.packages; [
+          # keep-sorted start
+          auto-tab-discard
+          bitwarden
+          blocktube
+          buster-captcha-solver
+          control-panel-for-twitter
+          cookies-txt
+          darkreader
+          dearrow
+          don-t-fuck-with-paste
+          header-editor
+          indie-wiki-buddy
+          justdeleteme
+          lovely-forks
+          search-by-image
+          sponsorblock
+          stylus
+          terms-of-service-didnt-read
+          translate-web-pages
+          ublacklist
+          ublock-origin
+          unpaywall
+          user-agent-string-switcher
+          userchrome-toggle-extended
+          violentmonkey
+          wayback-machine
+          web-scrobbler
+          zotero-connector
+          # keep-sorted end
+        ];
 
-        mkSearch = alias: domain: mkSearch' alias domain {};
-      in {
-        default = "ddg";
-        force = true;
+        search = let
+          mkSearchFull = {
+            alias,
+            domain,
+            searchPath ? "/search",
+            searchParam ? "q",
+            extraParams ? [],
+          }: {
+            icon = "https://" + domain + "/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000;
 
-        # TODO: i also don't really want some of these
-        engines = {
-          "bing".metaData.hidden = true;
-          "amazondotcom-us".metaData.hidden = true;
-          "ebay".metaData.hidden = true;
-
-          "mynixos" = mkSearch "mynix" "mynixos.com";
-          "github" = mkSearch "gh" "github.com";
-          "mdn" = mkSearch "mdn" "developer.mozilla.org";
-          "lastfm" = mkSearch "lastfm" "last.fm";
-          "effectindex" = mkSearch "effect" "effectindex.com";
-          "annasarchive" = mkSearch "aa" "annas-archive.org";
-
-          "youtube" = mkSearch' "yt" "youtube.com" {searchPath = "/results";};
-          "googlescholar" = mkSearch' "scholar" "scholar.google.com" {searchPath = "/scholar";};
-
-          "rym" = mkSearch' "rym" "rateyourmusic.com" {searchParam = "searchterm";};
-          "thedrugclassroom" = mkSearch' "tdc" "thedrugclassroom.com" {searchParam = "s";};
-
-          "e621" = mkSearch' "e621" "e621.net" {
-            searchPath = "/posts";
-            searchParam = "tags";
-          };
-
-          "psychonaut" = mkSearch' "psy" "psychonautwiki.org" {
-            searchPath = "/w/index.php";
-            searchParam = "search";
-          };
-
-          "tripsit" = mkSearch' "tripsit" "wiki.tripsit.me" {
-            searchPath = "/index.php";
-            searchParam = "search";
-          };
-
-          "noogle" = mkSearch' "noogle" "noogle.dev" {
-            searchPath = "/q";
-            searchParam = "term";
-          };
-
-          "nüschtos" = mkSearch' "nüschtos" "search.xn--nschtos-n2a.de" {
-            searchPath = "/";
-            searchParam = "query";
-          };
-
-          "searchix" = mkSearch' "searchix" "searchix.ovh" {
-            searchPath = "/";
-            searchParam = "query";
-          };
-
-          "nixpkgs" = mkSearch' "nix" "search.nixos.org" {
-            searchPath = "/packages";
-            searchParam = "query";
-            extraParams = [
+            definedAliases = ["@${alias}"];
+            urls = [
               {
-                name = "channel";
-                value = "unstable";
+                template = "https://" + domain + searchPath;
+                params =
+                  [
+                    {
+                      name = searchParam;
+                      value = "{searchTerms}";
+                    }
+                  ]
+                  ++ extraParams;
               }
             ];
           };
 
-          "erowid" = mkSearch' "erowid" "erowid.org" {
-            searchPath = "/experiences/exp.cgi";
-            searchParam = "Str";
-            extraParams = [
-              {
-                name = "A";
-                value = "Search";
+          mkSearch' = alias: domain: extra:
+            mkSearchFull ({
+                inherit alias domain;
               }
-            ];
-          };
+              // extra);
 
-          "msj" = mkSearch' "msj" "macserialjunkie.com" {
-            searchPath = "/forum/search.php";
-            searchParam = "keywords";
-            extraParams = [
-              {
-                name = "sf";
-                value = "titleonly";
-              }
-              {
-                name = "sr";
-                value = "topics";
-              }
-            ];
-          };
+          mkSearch = alias: domain: mkSearch' alias domain {};
+        in {
+          default = "ddg";
+          force = true;
 
-          "csrinru" = mkSearch' "rin" "cs.rin.ru" {
-            searchPath = "/forum/search.php";
-            searchParam = "keywords";
-            extraParams = [
-              {
-                name = "sf";
-                value = "titleonly";
-              }
-              {
-                name = "sr";
-                value = "topics";
-              }
-            ];
+          # TODO: i also don't really want some of these
+          engines = {
+            "bing".metaData.hidden = true;
+            "amazondotcom-us".metaData.hidden = true;
+            "ebay".metaData.hidden = true;
+
+            "mynixos" = mkSearch "mynix" "mynixos.com";
+            "github" = mkSearch "gh" "github.com";
+            "mdn" = mkSearch "mdn" "developer.mozilla.org";
+            "lastfm" = mkSearch "lastfm" "last.fm";
+            "effectindex" = mkSearch "effect" "effectindex.com";
+            "annasarchive" = mkSearch "aa" "annas-archive.org";
+
+            "youtube" = mkSearch' "yt" "youtube.com" {searchPath = "/results";};
+            "googlescholar" = mkSearch' "scholar" "scholar.google.com" {searchPath = "/scholar";};
+
+            "rym" = mkSearch' "rym" "rateyourmusic.com" {searchParam = "searchterm";};
+            "thedrugclassroom" = mkSearch' "tdc" "thedrugclassroom.com" {searchParam = "s";};
+
+            "e621" = mkSearch' "e621" "e621.net" {
+              searchPath = "/posts";
+              searchParam = "tags";
+            };
+
+            "psychonaut" = mkSearch' "psy" "psychonautwiki.org" {
+              searchPath = "/w/index.php";
+              searchParam = "search";
+            };
+
+            "tripsit" = mkSearch' "tripsit" "wiki.tripsit.me" {
+              searchPath = "/index.php";
+              searchParam = "search";
+            };
+
+            "noogle" = mkSearch' "noogle" "noogle.dev" {
+              searchPath = "/q";
+              searchParam = "term";
+            };
+
+            "nüschtos" = mkSearch' "nüschtos" "search.xn--nschtos-n2a.de" {
+              searchPath = "/";
+              searchParam = "query";
+            };
+
+            "searchix" = mkSearch' "searchix" "searchix.ovh" {
+              searchPath = "/";
+              searchParam = "query";
+            };
+
+            "nixpkgs" = mkSearch' "nix" "search.nixos.org" {
+              searchPath = "/packages";
+              searchParam = "query";
+              extraParams = [
+                {
+                  name = "channel";
+                  value = "unstable";
+                }
+              ];
+            };
+
+            "erowid" = mkSearch' "erowid" "erowid.org" {
+              searchPath = "/experiences/exp.cgi";
+              searchParam = "Str";
+              extraParams = [
+                {
+                  name = "A";
+                  value = "Search";
+                }
+              ];
+            };
+
+            "msj" = mkSearch' "msj" "macserialjunkie.com" {
+              searchPath = "/forum/search.php";
+              searchParam = "keywords";
+              extraParams = [
+                {
+                  name = "sf";
+                  value = "titleonly";
+                }
+                {
+                  name = "sr";
+                  value = "topics";
+                }
+              ];
+            };
+
+            "csrinru" = mkSearch' "rin" "cs.rin.ru" {
+              searchPath = "/forum/search.php";
+              searchParam = "keywords";
+              extraParams = [
+                {
+                  name = "sf";
+                  value = "titleonly";
+                }
+                {
+                  name = "sr";
+                  value = "topics";
+                }
+              ];
+            };
           };
         };
       };
