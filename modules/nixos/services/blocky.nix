16be5b08c1533ac84efc2f0992c03f68dcedc294
quaoz <74419801+quaoz@users.noreply.github.com>
2025-12-05 01:20:06 +0000

    feat: rework dns blocklists

--- modules/nixos/services/blocky.nix
+++ modules/nixos/services/blocky.nix
@@ -93,55 +93,65 @@ in {
               strategy = "fast";
             };
 
-            clientGroupsBlock = {
-              default = [
-                "multi"
-                "ads"
-                "tracking"
-                "malicious"
-              ];
-            };
+            clientGroupsBlock =
+              {
+                default = [
+                  "base"
+                  "normal"
+                ];
+              }
+              // (self.lib.hosts self {includeDarwin = true;}
+                |> lib.mapAttrs' (
+                  _hn: hc: {
+                    name = "${hc.config.networking.hostName}*";
+                    value = ["base" "normal" "big"];
+                  }
+                ));
 
-            # mainly sourced from https://firebog.net/
+            # sources:
+            #   - https://github.com/StevenBlack/hosts
+            #   - https://github.com/hagezi/dns-blocklists
+            #   - https://oisd.nl
+            #   - https://firebog.net/
+            #   - https://github.com/badmojr/1Hosts
+            #   - https://github.com/PrimePoobah/Pi-hole-Blocklist-Catalog
+            #
+            # while IFS= read -r url; do echo "$url"; curl -fsSL "$url" | rg '<domain>'; done < <(nix eval --raw --impure --expr '(builtins.getFlake "'$PWD'").nixosConfigurations.ganymede.config.services.blocky.settings.blocking.denylists |> builtins.attrValues |> builtins.concatLists |> builtins.concatStringsSep "\n"')
             denylists = {
-              multi = [
-                "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
-                "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts"
-                "https://v.firebog.net/hosts/static/w3kbl.txt"
+              base = [
                 "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-              ];
-
-              ads = [
-                "https://adaway.org/hosts.txt"
-                "https://v.firebog.net/hosts/AdguardDNS.txt"
-                "https://v.firebog.net/hosts/Admiral.txt"
-                "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
-                "https://v.firebog.net/hosts/Easylist.txt"
-                "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
-                "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
-                "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
-              ];
-
-              tracking = [
-                "https://v.firebog.net/hosts/Easyprivacy.txt"
-                "https://v.firebog.net/hosts/Prigent-Ads.txt"
-                "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
-                "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
-                "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
-              ];
-
-              malicious = [
-                "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
-                "https://v.firebog.net/hosts/Prigent-Crypto.txt"
-                "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
-                "https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt"
-                "https://phishing.army/download/phishing_army_blocklist_extended.txt"
-                "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"
-                "https://v.firebog.net/hosts/RPiList-Malware.txt"
-                "https://raw.githubusercontent.com/Spam404/lists/master/main-blacklist.txt"
-                "https://raw.githubusercontent.com/AssoEchap/stalkerware-indicators/master/generated/hosts"
+                "https://raw.githubusercontent.com/badmojr/1Hosts/master/Lite/wildcards.txt"
                 "https://urlhaus.abuse.ch/downloads/hostfile/"
-                "https://lists.cyberhost.uk/malware.txt"
+                "https://threatfox.abuse.ch/downloads/hostfile/"
+                "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
+                "https://raw.githubusercontent.com/blocklistproject/Lists/master/phishing.txt"
+                "https://adaway.org/hosts.txt"
+                "https://raw.githubusercontent.com/blocklistproject/Lists/master/crypto.txt"
+                "https://v.firebog.net/hosts/Prigent-Crypto.txt"
+              ];
+
+              small = [
+                "https://codeberg.org/hagezi/mirror2/raw/branch/main/dns-blocklists/wildcard/multi.txt"
+                "https://small.oisd.nl/domainswild"
+              ];
+
+              normal = [
+                "https://codeberg.org/hagezi/mirror2/raw/branch/main/dns-blocklists/wildcard/pro.txt"
+                "https://big.oisd.nl/domainswild"
+                "https://phishing.army/download/phishing_army_blocklist_extended.txt"
+                "https://raw.githubusercontent.com/blocklistproject/Lists/master/ransomware.txt"
+              ];
+
+              big = [
+                "https://codeberg.org/hagezi/mirror2/raw/branch/main/dns-blocklists/wildcard/tif.txt"
+                "https://codeberg.org/hagezi/mirror2/raw/branch/main/dns-blocklists/wildcard/fake.txt"
+                "https://raw.githubusercontent.com/blocklistproject/Lists/master/scam.txt"
+                "https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt"
+                "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/easylist"
+                "https://raw.githubusercontent.com/RPiList/specials/master/Blocklisten/Win10Telemetry"
+                "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+                "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
+                "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
               ];
             };
           };
