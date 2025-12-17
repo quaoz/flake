3d90afd64553cb994a454f845ce3984fb9c001e6
quaoz <74419801+quaoz@users.noreply.github.com>
2025-12-17 17:08:27 +0000

    nixos/security/pam: enable pam u2f

--- modules/nixos/security/pam.nix
+++ modules/nixos/security/pam.nix
@@ -1,9 +1,28 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   security = {
-    # use ssh keys to authenticate when on a remote connection
     pam = {
-      sshAgentAuth.enable = true;
+      # https://wiki.nixos.org/wiki/Yubikey
+      # https://joinemm.dev/blog/yubikey-nixos-guide
+      u2f = lib.mkIf config.garden.hardware.yubikey.enable {
+        enable = true;
+        settings = {
+          cue = true;
+          origin = "pam://yubikey";
 
+          # nix run nixpkgs#pam_u2f -- --pin-verification --origin pam://yubikey
+          authfile =
+            pkgs.writeText "u2f-mappings"
+            "${config.me.username}:luC2WDEM54XrW6eYYKIG2YEDZXuV78lgesA0GA6klfHe98Cip/fXru+64TqROpBETZxdLB6ThwbT1AAqbU496g==,eCKjK8K8gtwGqViD74PniYXYgE0n1mBTB4FjElIgNf85zg+PN8MyUdzLBCVJk0t/qVvFnywztB6h2vmCSrR+hQ==,es256,+presence+pin";
+        };
+      };
+
+      # use ssh keys to authenticate when on a remote connection
+      sshAgentAuth.enable = true;
       services = {
         sudo.sshAgentAuth = true;
         sudo-rs.sshAgentAuth = true;
