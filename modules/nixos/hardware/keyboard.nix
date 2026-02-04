6949e6c4d0b56bc92085444a8ddfbea31e348671
quaoz <74419801+quaoz@users.noreply.github.com>
2026-02-04 16:38:05 +0000

    nixos/hardware/keyboard: init

---
+++ modules/nixos/hardware/keyboard.nix
@@ -0,0 +1,29 @@
+{
+  self,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.garden.hardware.keyboard;
+in {
+  options.garden.hardware.keyboard = {
+    layout = self.lib.mkOpt (lib.types.enum ["gb" "us"]) "gb" "The keyboard layout to use";
+    apple = lib.mkEnableOption "apple keyboard configuration";
+  };
+
+  config = {
+    # https://wiki.archlinux.org/title/Apple_Keyboard#hid_apple_module_options
+    boot.extraModprobeConfig = lib.optionalString cfg.apple ''
+      options hid_apple iso_layout=${
+        if cfg.layout == "gb"
+        then "1"
+        else "0"
+      }
+    '';
+
+    services.xserver.xkb = {
+      layout = cfg.layout;
+      variant = lib.optionalString cfg.apple "mac";
+    };
+  };
+}
