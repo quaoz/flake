2e8f13b79399a88890d14849860b8c10826e1621
quaoz <74419801+quaoz@users.noreply.github.com>
2026-02-04 16:47:07 +0000

    user/desktop/bitwarden: enable cli and remove ssh-agent

--- user/desktop/bitwarden.nix
+++ user/desktop/bitwarden.nix
@@ -1,26 +1,14 @@
 {
   lib,
-  config,
   osConfig,
   pkgs,
   ...
-}: let
-  inherit (config.home) homeDirectory;
-in {
+}: {
   # password manager
   home = lib.mkIf osConfig.garden.profiles.desktop.enable {
     packages = with pkgs; [
       bitwarden-desktop
-      # WATCH: https://github.com/NixOS/nixpkgs/issues/339576
-      # bitwarden-cli
+      bitwarden-cli
     ];
-
-    sessionVariables = {
-      # TODO: don't really like using ssh-agent, only really need it for deploy-rs
-      #       maybe figure out something else or just remove password requirement
-      #       for wheel
-      # use bitwarden ssh-agent
-      SSH_AUTH_SOCK = "${homeDirectory}/.bitwarden-ssh-agent.sock";
-    };
   };
 }
